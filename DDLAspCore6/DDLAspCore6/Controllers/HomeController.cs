97f5e12273f33ef4d8d32953cfbbb325de3890d2
ashishsenapati4 <ashissenapati111@gmail.com>
2025-09-30 21:34:32 +0530

    DDLAspCore6/ added

---
+++ DDLAspCore6/DDLAspCore6/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using DDLAspCore6.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DDLAspCore6.Controllers
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            List<SelectListItem> list = new List<SelectListItem>
+            {
+                new SelectListItem("Male","Male"),
+                new SelectListItem("Female", "Female"),
+                new SelectListItem("Other", "Other")
+            };
+            ViewBag.List = list;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
